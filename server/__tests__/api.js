5cd60f17a577d7ca907c5a44a796c9410c9c3ecd
Kyole <briankyole10@gmail.com>
2020-12-11 19:53:37 +0300

    fixing bugs in tests

--- server/__tests__/api.js
+++ server/__tests__/api.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 require("dotenv").config();
 const configValues = process.env;
 
-const url = `${configValues.TEST_URL}/graphql`;
+const url = "http://localhost:4003/graphql";
 
 const signIn = async variables =>
 await axios.post(url,{
