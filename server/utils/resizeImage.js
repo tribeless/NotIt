b8e3f983b996c4a8cd510cecd55b3004ab41f3e7
Kyole <briankyole10@gmail.com>
2021-01-21 15:13:02 +0300

    changed from using sharp to jimp in order to resize images

--- server/utils/resizeImage.js
+++ server/utils/resizeImage.js
@@ -1,4 +1,5 @@
 const sharp = require("sharp");
+const Jimp = require('jimp');
 const shortId = require("shortid");
 const path = require("path");
 const createResizeFile = require("./createResizeFile");
@@ -6,23 +7,49 @@ const Logger = require("./logger");
 
 const resizeImage = async(image,outputDir)=>{
     const newFileName = `${shortId.generate()}.png`;
-    await sharp(image).resize(50,40,{
-        fit:"inside"
-    }).toBuffer().then((data)=>{
-        const resizedImage = path.join(outputDir,newFileName);
-        createResizeFile(resizedImage,data);
-    })
-    .catch(e=>{
-        const customerMessage = "Could not resize your image";
-        Logger.log(
-            'error',
-            'Error', {
-                message: e.message
-            }
-        )
-        throw new Error(e);
-    })
-    return newFileName;
+    //Could not use sharp to resize image because of Error: " Input file contains unsupported image format"
+    // while resizing
+    // see sharp issues https://github.com/lovell/sharp/issues/1255
+
+    // await sharp(image).resize(50,40,{
+    //     fit:"inside"
+    // }).toBuffer().then((data)=>{
+    //     const resizedImage = path.join(outputDir,newFileName);
+    //     createResizeFile(resizedImage,data);
+    // })
+    // .catch(e=>{
+    //     const customerMessage = "Could not resize your image";
+    //     Logger.log(
+    //         'error',
+    //         'Error', {
+    //             message: e.message
+    //         }
+    //     )
+    //     throw new Error(e);
+    // });
+
+      try {
+          // Read the image.
+          const image = await Jimp.read(image);
+          // contain the image to the provided width and height.
+          // we will get the center part of the image which is the most significant
+          // eslint-disable-next-line no-bitwise
+          await image.contain(50, 40, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+          // Save and overwrite the image
+          const resizedImage = path.join(outputDir, newFileName);
+          await image.writeAsync(resizedImage);
+          return newFileName;
+      } catch (e) {
+            const customerMessage = "Could not resize your image";
+            Logger.log(
+                'error',
+                'Error', {
+                    message: e.message
+                }
+            )
+            throw new Error(e);
+      }
+    
 }
 
 module.exports = resizeImage;
